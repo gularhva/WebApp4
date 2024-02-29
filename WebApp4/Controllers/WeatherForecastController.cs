b9549bdc1c6855fda585e08c0c9a4a9e41df673c
gularhuseynova777@gmail.com <gularhuseynova777@gmail.com>
2024-02-29 23:12:05 +0400

    new changes

--- WebApp4/Controllers/WeatherForecastController.cs
+++ WebApp4/Controllers/WeatherForecastController.cs
@@ -9,21 +9,6 @@ namespace WebApp4.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private readonly IStudentService _studentService;
-        public WeatherForecastController(IStudentService studentService)
-        {
-            _studentService = studentService;
-        }
-        [HttpGet]
-        public Task<GenericResponseModel<List<StudentGetDTO>>> Get()
-        {
-            var data = _studentService.GetAllStudents();
-            return data;
-        }
-        [HttpPost]
-        public Task<GenericResponseModel<StudentCreateDTO>> AddStudent(StudentCreateDTO model)
-        {
-            return _studentService.AddStudent(model);
-        }
+        
     }
 }
